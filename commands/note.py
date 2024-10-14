76340cae64c3327152ba594041d2d4de843b0577
Kevin Dohmen <kevin.dohmen@twintos.com>
2024-10-14 16:03:07 +0200

    added note command

---
+++ commands/note.py
@@ -0,0 +1,76 @@
+import os
+import argparse
+import json
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+def getCache() -> dict:
+    try:
+        with open(f"../pt-cache/notecache.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+
+def saveCache(cache: dict):
+    if not os.path.exists(f"../pt-cache"):
+        os.makedirs(f"../pt-cache")
+    with open(f"../pt-cache/notecache.json", "w") as file:
+        json.dump(cache, file)
+
+def addNote(noteName: str, note: str, append: bool = False):
+    cache = getCache()
+    name = noteName if noteName else "default"
+    if cache:
+        if append and name in cache:
+            cache[name] += f"\\n{note}"
+        else:
+            cache[name] = note
+    else:
+        cache = {name: note}
+    saveCache(cache)
+
+def addIndentation(text:str, indentation:int):
+    return "\n".join([f"{' '*indentation}{line}" for line in text.split("\\n")])
+
+def listNotes(noteName: str = None):
+    cache = getCache()
+    if cache:
+        if noteName:
+            print(f"Note: {noteName}")
+            print(addIndentation(str(cache[noteName]), 2))
+        else:
+            for noteName, note in cache.items():
+                print(f"Note: {noteName}")
+                print(addIndentation(str(note), 2))
+                print()
+
+def run(raw_args):
+    parser = argparse.ArgumentParser(
+        description="take some simple notes"
+    )
+    parser.add_argument("-l", "--list", help="list all notes", action="store_true")
+    parser.add_argument("-d", "--delete", help="delete a note", type=str)
+    parser.add_argument("-a", "--add", help="add a note", action="store_true")
+    parser.add_argument("note", nargs="*", help="note name")
+    args = parser.parse_args(raw_args)
+
+    if len(args.note) == 1 and not args.list and not args.delete:
+        listNotes(args.note[0])
+    elif args.note:
+        addNote(args.note[0], "\\n".join(args.note[1:]), append=args.add)
+    elif args.list:
+        listNotes()
+    elif args.delete:
+        cache = getCache()
+        if cache:
+            if args.delete in cache:
+                del cache[args.delete]
+                saveCache(cache)
+            else:
+                print(f"Note {args.delete} not found")
+        else:
+            print("No notes found")
+    else:
+        listNotes()
+    
