d04fae07b2ae5ba346e0f99c16e1a8435b743811
David van Hoek <david.vanhoek@twintos.com>
2024-10-31 11:26:43 +0100

    added run command, set in profile browser with tabs and cmd tabs to start

---
+++ commands/run.py
@@ -0,0 +1,131 @@
+import argparse
+import os
+import json
+import subprocess
+
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(script_dir)
+
+def getCache():
+    try:
+        with open(f"../pt-cache/profiles.json", "r") as file:
+            return json.load(file)
+    except FileNotFoundError:
+        return None
+
+def saveCache(cache):
+    if not os.path.exists(f"../pt-cache"):
+        os.makedirs(f"../pt-cache")
+    with open(f"../pt-cache/profiles.json", "w") as file:
+        json.dump(cache, file)
+
+def runCommand(command):  
+    # Run the command asynchronously
+    try:
+        # Use `shell=True` to execute it as a command line string on Windows
+        process = subprocess.Popen(
+            command,
+            shell=True,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE
+        )
+        
+        # Collect standard output and error
+        stdout, stderr = process.communicate()
+        
+        # Check for errors in stderr
+        if stderr:
+            print(f"Error running command: {stderr.decode()}")
+        else:
+            print("command ran successfully.")
+    except Exception as e:
+        print(f"An exception occurred: {e}")
+
+def loadProfile(profileName:str):
+    profiles = getCache()
+    try:
+        return Profile(True,profiles["profiles"][profileName])
+    except KeyError:
+        return Profile(False,"profile does not exist")
+
+class Profile:
+    loadedCorrectly = False
+    profile= ""
+    def __init__(self,LoadedCorrectly,profile):
+        self.LoadedCorrectly =LoadedCorrectly
+        self.profile = profile
+
+class BrowserTask:
+    BroswerName = "msedge"
+    Tabs = []
+    def __init__(self,task):
+        try:
+            self.BrowserName = task["browser"]; 
+        except:
+            print("could not load broswer name correctly, make sure to include a broswer name\n defaulting to msedge")
+        try:
+            for tab in task["tabs"]:
+                tab = tab.replace("%20"," ")
+                self.Tabs.append(tab)
+        except:
+            print("could not load tab list correctly, make sure to give a list of tabs\n defaulting to opening no tabs")        
+
+class CMDTask:
+    TerminalTabs = []
+    def __init__(self,task):
+        try:
+            for window in task["windows"]:
+                newtab = CMDTab(window["name"],window["path"],window["commands"])
+                self.TerminalTabs.append(newtab)
+        except:
+            self.TerminalTabs.append(CMDTab("Default","C:\\",["echo Default tab in case no tabs were given/loaded"]))
+            print("failed to load tabs")
+
+class CMDTab:
+    Name = ""
+    Path = ""
+    commands = []
+    def __init__(self,name,path,commands):
+        self.Name = name
+        self.Path = path
+        self.commands = commands
+
+
+def runCMD(task):
+    cmdTask = CMDTask(task)
+    command = "wt"
+    for tab in cmdTask.TerminalTabs:
+        command += f" nt -p \"Command Prompt\" --title \"{tab.Name}\" -d \"{tab.Path}\" cmd /k \""
+        for com in tab.commands:
+            command += f"{com} && "
+        command = command[:-3]
+        command += "\";"
+    command = command[:-1]
+    runCommand(command)
+
+def runBrowser(task):
+    command = ["start", task["browser"]]
+    browserTask = BrowserTask(task)
+    for tab in browserTask.Tabs:
+        command.append(tab)
+    # runCommand(command)
+
+
+def run(args):
+    parser = argparse.ArgumentParser(
+        description="Run Command, runs a set profile. a profile contains one or more programs to start"
+    )
+    parser.add_argument("-p", "--profile",required=True, help="Show detailed help")
+    parsed_args = parser.parse_args(args)
+    profile = loadProfile(parsed_args.profile)
+    if profile.LoadedCorrectly == False:
+        print("Failed to load profile")
+        return profile.profile
+    for task in profile.profile:
+        match task["type"]:
+            case "cmd":
+                runCMD(task)
+            case "browser":
+                runBrowser(task)
+    
