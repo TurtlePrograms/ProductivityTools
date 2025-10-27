48a6d390fb5e34dbabe0dea575d3f4451af812d6
David van Hoek <turtleprogramsmail@gmail.com>
2025-10-27 12:11:13 +0100

    feat: add branch option to git open tool

--- tools/git_tools/open.py
+++ tools/git_tools/open.py
@@ -2,14 +2,32 @@ import argparse
 import webbrowser
 from tools.core import ToolRegistry, GitClient, Logger, LogLevel
 
+
 def run(args):
     parser = argparse.ArgumentParser(
-        description=ToolRegistry.getToolDescription("open","git")
+        description=ToolRegistry.getToolDescription("open", "git")
     )
+    parser.add_argument(
+        "-b",
+        "--branch",
+        type=str,
+        nargs="?",
+        const="CURRENT_BRANCH",  # Used if -b is provided but no value
+        help="open git to this branch",
+    )
+
     parsed_args = parser.parse_args(args)
-    webbrowser.open_new_tab(GitClient.getRemoteUrl())
-    return
+    url = GitClient.getRemoteUrl()
+
+    if parsed_args.branch is not None:
+        if parsed_args.branch == "CURRENT_BRANCH":
+            branch = GitClient.getCurrentBranch()
+        else:
+            branch = parsed_args.branch
+        url = url + "/tree/" + branch
+
+    webbrowser.open_new_tab(url)
+
 
 if __name__ == "__main__":
     Logger.log("Cannot run this tool directly", LogLevel.CRITICAL)
-
